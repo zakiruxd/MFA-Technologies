4317ca7df1eecdfc486ea400390d1d6f005675ca
Zakir Faizal <zakiruxd@gmail.com>
2021-02-24 16:43:13 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+
+//     $(document).ready(function () {
+//         var url = window.location;
+//         $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+//         $('ul.nav a').filter(function () {
+//             return this.href == url;
+//         }).parent().addClass('active');
+//     });
+// });
+
